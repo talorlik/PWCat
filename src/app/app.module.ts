776150a18025ca6d5961a901615ee31a4fbb1826
Tal Orlik <talo@vatit.com>
2018-02-15 12:18:54 +0200

    First commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { MatToolbarModule, MatCardModule, MatButtonModule } from "@angular/material";
+
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
